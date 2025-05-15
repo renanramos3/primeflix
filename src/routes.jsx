48ebfcdf18f3019f2c12acaeb99ed6abf4ef692d
renanramos3 <renanramos3@gmail.com>
2025-05-14 21:00:55 -0300

    Corrige importação do componente Favoritos

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
-import Favoritos from "./pages/favoritos";
+import Favoritos from "./pages/Favoritos";
 
 import Erro from "./pages/Erro";
 
